6f60387f3fbbfe75837c760ebf4f3ab82fc8f4fe
NatalyaKhrol <tashapas@yahoo.com>
2022-10-17 20:28:28 +0300

    Test for Rating is added

--- src/test/java/pages/ResultPage.java
+++ src/test/java/pages/ResultPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class ResultPage extends BasePage {
     //private final By SORT_BUTTON = By.cssSelector("[data-testid=sorters-dropdown-trigger]");
-    private final By HOTEL_RATING = By.xpath("//div[@data-testid='review-score']/div");
+    private final By HOTEL_RATING = By.xpath("(//div[@data-testid='review-score']/div)[1]");
 
 
     public ResultPage(WebDriver driver) {
--- src/test/resources/features/search.feature
+++ src/test/resources/features/search.feature
@@ -3,5 +3,5 @@ Feature: Search on booking.com
   Scenario: Name and hotel rating should be correct after search (Dubai)
     Given user is on the search page
     When user searches for "Dubai"
-    Then hotel name is "Flora Al Barsha Hotel At The Mall"
-    And hotel rating is "8.2"
+    Then hotel name is "MD Hotel By Gewan formerly Cassells"
+    And hotel rating is "7,6"
